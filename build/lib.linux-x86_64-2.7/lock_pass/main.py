45badbb0a855c4b06205a082d8837731f2f131fd
ShitalLuitel <ctalluitel@gmail.com>
2017-09-03 01:00:14 +0545

    v1.9

--- build/lib.linux-x86_64-2.7/lock_pass/main.py
+++ build/lib.linux-x86_64-2.7/lock_pass/main.py
@@ -56,11 +56,11 @@ def set_up():
         print "**File Not found**"
         print "******************\n"
         print "Creating file",
-        for i in range(randint(1, 6)):
+        for i in range(randint(1, 10)):
             print ".",
             time.sleep(1)
-        print
-        print "File created."
+
+        print "\nFile created."
         f_password.close()
         f_username.close()
     else:
@@ -86,22 +86,23 @@ def run():
 
             print save(i_username,i_password)
 
-            s_choice = raw_input("\nDo You Want To Continue (y/n): ")
-
-            if s_choice.lower() == 'y':
-                continue
-            else:
-                while True:
-                    s_choice = raw_input("\n1. To Retrive Your Password \n2. Exit \nChoice")
-                    if int(s_choice) == 1:
-                        choice = 2
-                        continue
-                    elif int(s_choice) == 2:
-                        print "!!!Thank You!!!"
-                        exit()
+            while True:
+                s_choice = raw_input("\nDo You Want To Continue (y/n): ")
+                if s_choice.lower() == 'y':
+                    break
+                elif s_choice.lower() == 'n':
+                    while True:
+                        s_choice = raw_input("\n1. To Retrive Your Password \n2. Exit \nChoice:")
+                        if int(s_choice) == 1:
+                            choice = 2
+                            break
+                        elif int(s_choice) == 2:
+                            print "!!!Thank You!!!"
+                            exit()
 
         elif choice == 2:
             user_dict = getdir()
+            print "\n**You are going to retrive your password**"
             r_username = raw_input("Enter username: ")
             while r_username not in user_dict.keys():
                 print "\n***Username not found***"
@@ -112,23 +113,25 @@ def run():
             spam = pyperclip.paste()
             print "Password \"%s\" for username \"%s\" copied to clipboard." %( "*" * len(r_password), r_username)
 
-            s_choice = raw_input("\nDo You Want To Continue (y/n): ")
-
-            if s_choice.lower() == 'y':
-                continue
-            else:
-                while True:
-                    s_choice = raw_input("\n1. To Add Username And Password \n2. Exit \nChoice: ")
-                    if int (s_choice) == 1:
-                        choice = 2
-                        continue
-                    elif int(s_choice) == 2:
-                        print "\n\n!!!Thank You!!!"
-                        exit()
+            while True:
+                s_choice = raw_input("\nDo You Want To Continue (y/n): ")
+                if s_choice.lower() == 'y':
+                    break
+                else:
+                    while True:
+                        s_choice = raw_input("\n1. To Add Username And Password \n2. Exit \nChoice: ")
+                        if int(s_choice) == 1:
+                            choice = 1
+                            break
+                        elif int(s_choice) == 2:
+                            print "\n\n!!!Thank You!!!"
+                            exit()
 
         else:
             print "\n\n!!!Thank You!!!"
             exit()
+
+
 
 
 
