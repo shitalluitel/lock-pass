250fbbe242ff34e50b00104f9c21f0bb11ffd701
ShitalLuitel <ctalluitel@gmail.com>
2017-09-02 22:36:13 +0545

    First commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+    
+# requirements = [pkg.split('=')[0] for pkg in open('requirements.txt').readlines()]
+
+# description = "Commandline tool to listen all radio stations of Nepal"
+
+# long_description = open("README.rst").read()
+
+classifiers = ['Environment :: Console',
+               'Programming Language :: Python :: 3'
+               ]
+
+# version = open('CHANGES.txt').readlines()[0][1:].strip()
+
+setup(name='lock-pass',
+      version= 1.0,
+      description= "To save username and password",
+      author='Shital Babu Luitel',
+      author_email='ctalluitel@gmail.com',
+      url='https://github.com/shitalluitel/',
+      scripts=['src/lock-pass',],
+      packages=['lock_pass'],
+      package_dir = {'lock_pass': 'src/lock_pass'},
+      classifiers=classifiers
+    )
---
+++ src/lock-pass
@@ -0,0 +1,3 @@
+if __name__ == "__main__":
+	from lock_pass.main import main
+	main()
